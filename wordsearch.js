a820a584752fe1111f3881de36c268e777cb06d3
Maheerah Anwar <maheerahanwar@gmail.com>
2024-11-17 08:12:09 -0500

    completed pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+///transpose function 
+const transpose = function (matrix) {
+    const reversArray = []; 
+    for(let i = 0; i < matrix[0].length; i++){ 
+      const newArr = [];
+     for(let j = 0; j < matrix.length; j++){  
+            newArr.push(matrix[j][i]); 
+        }
+        reversArray.push(newArr);
     }
-}
+  return reversArray;
+  };
 
-module.exports = wordSearch
+const wordSearch = (letters, word, callback) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  if (callback(horizontalJoin, word)) {
+    return true;
+  } else {
+    let vertical = transpose(letters);
+    const verticalJoin = vertical.map(ls => ls.join(''));
+    console.log(verticalJoin)
+    if (callback(verticalJoin, word))
+      return true;
+  }
+  return false;
+};
+
+// callback -> string, word
+
+const checkString = (string, word) => {
+  for (let l of string) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = { wordSearch, checkString };
